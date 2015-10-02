7f18b079ec8660b45cf55e168e81b76126bddfae
Lucas Mouilleron <lucas.mouilleron@me.com>
2015-10-02 10:41:07 +0200

    jwt

---
+++ README.md
@@ -0,0 +1,21 @@
+sparkBoilerplate
+================
+
+Authentication
+--------------
+- JWT
+- Get token : `curl -k https://localhost:8080/login/ilovekate`
+- Acces restricted area : `curl -k --header "token: eyJhbGciOiJSUzI1NiJ9.MTQ0Mzc3MzUzNDMzMA.d4XJ_zXYBAXxaM6UYgHUnwEpxPSh2Hv4yp0XVvZwM9PTPHgl0b3Qy8SseK7nVTWehMeHXo5XJrTdxZF0c9BWH_-27wnu3t54Z8zsRZnm4Ies07VXRIbOy-p1U0UzPwX24TqkzQnDX5poIrVrbJcxoqmRCvgBxpx06hqtNja0CU4xH2Ezk1wwSdKdWtQOHVhGvZyAaWYts9S80fyE4wY7i0uDri2vt3mzhN6Z2leXWsZABiVH85V-sNmIhUAU-bclXYWJ-HbFytzSBMpv9t9QaJyFnub-y3MfVMQGudnijd6io_SlbelkWWv2eiu2XSmEZttr0zrsaklD1P-_lLYCvQ" https://localhost:8080/protected`
+
+SSL
+---
+- Generate keystore : 
+    - `keytool -genkey -keyalg RSA -alias sparkBoilerplate -keystore keystore -validity 3600 -keysize 2048`
+    - "What is your first and last name?" has to be the domain name
+- Self signing : `keytool -export -alias sparkBoilerplate -file certificate.crt -keystore keystore`
+- CA signing : 
+    - Generate CSR (certificate request) : `keytool -keystore keystore -certreq -alias sparkBoilerplate -keyalg rsa -file certificate.csr`
+    - Get from CA the certificate.crt file
+- Generate trustore : `keytool -import -file certificate.crt -alias sparkBoilerplate -keystore truststore`
+- Test keystore : `keytool -list -v -keystore keystore`
+- In this boilerplate, password is `password` and host is `localhost`
--- src/sparkboilerplate/SparkBoilerplate.java
+++ src/sparkboilerplate/SparkBoilerplate.java
@@ -1,11 +1,25 @@
 package sparkboilerplate;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
+import ch.qos.logback.classic.Logger;
+import com.google.gson.Gson;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+
+import org.slf4j.LoggerFactory;
+import spark.ExceptionHandler;
+import spark.Filter;
 import spark.Request;
 import spark.Response;
+import spark.ResponseTransformer;
 import spark.Route;
 import spark.Spark;
 
@@ -13,12 +27,84 @@ public class SparkBoilerplate
 {
 
     ////////////////////////////////////////////////////////////////////////////////
+    public static final int REST_PORT = 8080;
+    public static final String REST_PASSWORD = "ilovekate";
+    public static final String JWT_PRIVATE_KEY_FILE = "./jwt/private_key.der";
+    public static final String JWT_PUBLIC_KEY_FILE = "./jwt/public_key.der";
+    public static final String SSL_KEYSTORE_FILE = "./ssl/keystore";
+    public static final String SSL_KEYSTORE_PASSWORD = "password";
+    public static final String SSL_TRUSTSTORE_FILE = "./ssl/truststore";
+    public static final String SSL_TRUSTSTORE_PASSWORD = "password";
+    ////////////////////////////////////////////////////////////////////////////////
+    public static PrivateKey JWTPrivateKey;
+    public static PublicKey JWTPublicKey;
+
+    ////////////////////////////////////////////////////////////////////////////////
     public static void main(String[] args)
     {
-        disableLog4J();
 
-        Spark.port(8080);
+        try
+        {
+            disableLog4J();
+            JWTPrivateKey = makeJWTPrivateKey();
+            JWTPublicKey = makeJWTPublicKey();
+            initSpark();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
 
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    private static void initSpark() throws Exception
+    {
+        // INIT
+        Spark.port(REST_PORT);
+        Spark.secure(SSL_KEYSTORE_FILE, SSL_KEYSTORE_PASSWORD, SSL_TRUSTSTORE_FILE, SSL_TRUSTSTORE_PASSWORD);
+
+        // FILTERS
+        Filter protectedFiler = new Filter()
+        {
+
+            @Override
+            public void handle(Request rqst, Response rspns) throws Exception
+            {
+                boolean authenticated = true;
+                String token = rqst.headers("token");
+                try
+                {
+                    checkSignString(token);
+                }
+                catch(Exception e)
+                {
+                    authenticated = false;
+                }
+                if(!authenticated)
+                {
+                    Spark.halt(401, "You are not welcome here");
+                }
+            }
+        };
+        Spark.before("/protected", protectedFiler);
+        Spark.before("/protected/*", protectedFiler);
+        Spark.before("/otherProtected", protectedFiler);
+
+        // EXCEPTIONS
+        Spark.exception(Exception.class, new ExceptionHandler()
+        {
+
+            @Override
+            public void handle(Exception excptn, Request rqst, Response rspns)
+            {
+                excptn.printStackTrace();
+                rspns.status(500);
+                rspns.body("Somethign wrong happened");
+            }
+        });
+
+        // ROUTES
         Spark.get("/hello", new Route()
         {
             @Override
@@ -27,16 +113,157 @@ public class SparkBoilerplate
                 return "Hello !!!";
             }
         });
+
+        Spark.get("/hello/:id", new Route()
+        {
+            @Override
+            public Object handle(Request rqst, Response rspns) throws Exception
+            {
+                return "Hello " + rqst.params(":id") + " !!!";
+            }
+        });
+
+        Spark.get("/objectToJson", new Route()
+        {
+            @Override
+            public Object handle(Request rqst, Response rspns) throws Exception
+            {
+                return new Person("lucas", "super");
+            }
+        }, new JsonTransformer());
+
+        Spark.get("/mapToJson", new Route()
+        {
+            @Override
+            public Object handle(Request rqst, Response rspns) throws Exception
+            {
+                HashMap map = new HashMap();
+                map.put("name", "lucas");
+                map.put("power", "super");
+                return map;
+            }
+        }, new JsonTransformer());
+
+        Spark.get("/redirect", new Route()
+        {
+            @Override
+            public Object handle(Request rqst, Response rspns) throws Exception
+            {
+                //throw new Exception();
+                rspns.redirect("/hello");
+                return null;
+            }
+        });
+
+        Spark.get("/login/:password", new Route()
+        {
+            @Override
+            public Object handle(Request rqst, Response rspns) throws Exception
+            {
+                String password = rqst.params(":password");
+                if(password.equals(REST_PASSWORD))
+                {
+                    return signString(Long.toString(System.currentTimeMillis()));
+                }
+                else
+                {
+                    throw new Exception("can't authentify");
+                }
+
+            }
+        });
+
+        Spark.get("/protected", new Route()
+        {
+            @Override
+            public Object handle(Request rqst, Response rspns) throws Exception
+            {
+                return "Private !!!";
+            }
+        });
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    private static String signString(String string) throws Exception
+    {
+        JsonWebSignature jws = new JsonWebSignature();
+        jws.setPayload(string);
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
+        jws.setKey(JWTPrivateKey);
+        return jws.getCompactSerialization();
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    private static String checkSignString(String string) throws Exception
+    {
+        JsonWebSignature jws = new JsonWebSignature();
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
+        jws.setKey(JWTPublicKey);
+        jws.setCompactSerialization(string);
+        return jws.getPayload();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    private static PrivateKey makeJWTPrivateKey() throws Exception
+    {
+        File f = new File(JWT_PRIVATE_KEY_FILE);
+        FileInputStream fis = new FileInputStream(f);
+        DataInputStream dis = new DataInputStream(fis);
+        byte[] keyBytes = new byte[(int) f.length()];
+        dis.readFully(keyBytes);
+        dis.close();
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePrivate(spec);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    private static PublicKey makeJWTPublicKey() throws Exception
+    {
+        File fPublic = new File("./jwt/public_key.der");
+        FileInputStream fisPublic = new FileInputStream(fPublic);
+        DataInputStream disPublic = new DataInputStream(fisPublic);
+        byte[] keyPublicBytes = new byte[(int) fPublic.length()];
+        disPublic.readFully(keyPublicBytes);
+        disPublic.close();
+        X509EncodedKeySpec specPublic = new X509EncodedKeySpec(keyPublicBytes);
+        KeyFactory kfPublic = KeyFactory.getInstance("RSA");
+        return kfPublic.generatePublic(specPublic);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    private static class Person
+    {
+
+        public String name;
+        public String power;
+
+        public Person(String name, String power)
+        {
+            this.name = name;
+            this.power = power;
+        }
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    private static class JsonTransformer implements ResponseTransformer
+    {
+
+        private Gson gson = new Gson();
+
+        @Override
+        public String render(Object model)
+        {
+            return gson.toJson(model);
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////
     private static void disableLog4J()
     {
-        System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "OFF");
-        List<String> loggers = Collections.<String>list(LogManager.getLogManager().getLoggerNames());
-        for(String logger : loggers)
-        {
-            LogManager.getLogManager().getLogger(logger).setLevel(Level.OFF);
-        }
+        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.setLevel(ch.qos.logback.classic.Level.ERROR);
     }
 }
